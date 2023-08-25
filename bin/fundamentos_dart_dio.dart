573c4b5033757fbe2c0fa6fa0501a568c87bb11d
Acássio Moura Vilela <maestro.etmaxx@gmail.com>
2023-08-24 21:05:04 -0300

    create project with Dart

---
+++ bin/fundamentos_dart_dio.dart
@@ -0,0 +1,5 @@
+import 'package:fundamentos_dart_dio/fundamentos_dart_dio.dart' as fundamentos_dart_dio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fundamentos_dart_dio.calculate()}!');
+}
---
+++ test/fundamentos_dart_dio_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundamentos_dart_dio/fundamentos_dart_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
